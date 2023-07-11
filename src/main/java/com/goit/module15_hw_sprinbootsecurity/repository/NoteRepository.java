3484d4b3fed7d0e86a7b6359a3aa1e65516f3cab
Svyatoslav Shypka <svyat200909@gmail.com>
2023-07-11 20:10:38 +0200

    Added test

--- src/main/java/com/goit/module15_hw_sprinbootsecurity/repository/NoteRepository.java
+++ src/main/java/com/goit/module15_hw_sprinbootsecurity/repository/NoteRepository.java
@@ -1,5 +1,6 @@
 package com.goit.module15_hw_sprinbootsecurity.repository;
 
+import com.goit.module15_hw_sprinbootsecurity.dto.NoteDto;
 import com.goit.module15_hw_sprinbootsecurity.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
---
+++ src/test/java/com/goit/module15_hw_sprinbootsecurity/Module16HwSpringBootSecurityCICDApplicationTest.java
@@ -0,0 +1,13 @@
+package com.goit.module15_hw_sprinbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class Module16HwSpringBootSecurityCICDApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
