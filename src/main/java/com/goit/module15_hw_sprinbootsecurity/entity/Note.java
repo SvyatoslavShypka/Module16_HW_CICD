bb2af244b81d2aab516030009ee0b371a34442c9
Svyatoslav Shypka <svyat200909@gmail.com>
2023-07-11 21:46:16 +0200

    Githu WorkFlow works

--- src/main/java/com/goit/module15_hw_sprinbootsecurity/entity/Note.java
+++ src/main/java/com/goit/module15_hw_sprinbootsecurity/entity/Note.java
@@ -1,12 +1,11 @@
 package com.goit.module15_hw_sprinbootsecurity.entity;
 
-import jakarta.persistence.*;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Table;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import org.springframework.stereotype.Component;
 
 
